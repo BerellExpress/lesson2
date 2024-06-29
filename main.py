21aa97c637450009a9b2bc529530e9ccaf66d377
BerellExpress <yury1997@mail.ru>
2024-06-30 00:16:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+name_of_course = 'Python'
+count_of_completed_homeworks = 12
+count_of_spent_hours = 1.5
+time_for_one_task = count_of_spent_hours / count_of_completed_homeworks
+print('Курс:', name_of_course, 'всего задач:', count_of_completed_homeworks, 'затрачено часов:', count_of_spent_hours, 'среднее время выполнения', time_for_one_task, 'часа')
